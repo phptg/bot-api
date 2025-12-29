34207c1b5b837205dabc36752291520411a5ad93
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-12-29 11:22:46 +0300

    phpdoc

--- src/Transport/TransportInterface.php
+++ src/Transport/TransportInterface.php
@@ -7,18 +7,45 @@ namespace Phptg\BotApi\Transport;
 use Phptg\BotApi\Type\InputFile;
 
 /**
+ * Interface for HTTP transport implementations.
+ *
+ * Defines the contract for making HTTP requests to the Telegram Bot API.
+ *
  * @api
  */
 interface TransportInterface
 {
+    /**
+     * Performs a GET request.
+     *
+     * @param string $url The URL to request.
+     *
+     * @return ApiResponse The API response.
+     */
     public function get(string $url): ApiResponse;
 
     /**
+     * Performs a POST request.
+     *
+     * @param string $url The URL to request.
+     * @param string $body The request body.
+     * @param string[] $headers The request headers as an associative array (header name => header value).
+     *
+     * @return ApiResponse The API response.
+     *
      * @psalm-param array<string, string> $headers
      */
     public function post(string $url, string $body, array $headers): ApiResponse;
 
     /**
+     * Performs a POST request with file uploads.
+     *
+     * @param string $url The URL to request.
+     * @param (bool|string|int|float)[] $data The form data fields as an associative array (field name => field value).
+     * @param InputFile[] $files The files to upload as an associative array (field name => `InputFile`).
+     *
+     * @return ApiResponse The API response.
+     *
      * @psalm-param array<string, scalar> $data
      * @psalm-param array<string, InputFile> $files
      */
