93f0d0843da513a8082ee142e5b33d72bd7bd011
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-02-07 22:05:08 +0300

    kill mutant

--- tests/Transport/InputFileDataTest.php
+++ tests/Transport/InputFileDataTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Phptg\BotApi\Tests\Transport;
 
 use LogicException;
+use Phptg\BotApi\Tests\Support\StubResourceReader;
 use Phptg\BotApi\Transport\InputFileData;
 use Phptg\BotApi\Type\InputFile;
 use PHPUnit\Framework\TestCase;
@@ -18,5 +19,16 @@ final class InputFileDataTest extends TestCase
         $this->expectException(LogicException::class);
         $this->expectExceptionMessage('No suitable resource reader found.');
         new InputFileData($file, []);
+    }
+
+    public function testExtensionAndBasenameAreNullForUri(): void
+    {
+        $reader = new StubResourceReader(uri: 'https://example.com/file.txt');
+
+        $file = new InputFile('resource');
+        $data = new InputFileData($file, [$reader]);
+
+        $this->assertNull($data->extension());
+        $this->assertNull($data->basename());
     }
 }
